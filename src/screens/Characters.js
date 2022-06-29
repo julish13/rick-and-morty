0a814221beffb707a0180a9143e4c148ddb48092
Julia <jkolupaeva@gmail.com>
2022-06-29 17:03:31 +0400

    fix: pagination bugs

--- src/screens/Characters.js
+++ src/screens/Characters.js
@@ -1,18 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useGetCharactersQuery } from '@redux';
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, useNavigate, useLocation } from 'react-router-dom';
 import { Pagination, CharactersList } from '@components';
 
 const Characters = () => {
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
   const searchParamsPage = Number(searchParams.get('page'));
+
   const initialPage = searchParamsPage || 1;
   const [page, setPage] = useState(initialPage);
   // const [query, setQuery] = useState(query);
+
   const { data, isLoading, error } = useGetCharactersQuery(
     page
     // query
   );
+
+  useEffect(() => {
+    if (!searchParams.get('page')) {
+      navigate(`${pathname}?page=${page}`, { replace: true });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (searchParamsPage) {
+      setPage(searchParamsPage);
+    }
+  }, [searchParamsPage]);
 
   if (isLoading) {
     return <p>is Loading...</p>;
