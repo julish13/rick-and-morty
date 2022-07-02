dd12212f2d1e9a33ec5bc1123aff8298fa7c7736
Julia <jkolupaeva@gmail.com>
2022-07-02 16:22:59 +0400

    Revert "fix: propTypes in Modal and LoadingSpinner"
    This reverts commit f5b1ad6e8f1a6ed24ff11b9a73346879c8f5df76.

--- src/components/Layout/LoadingSpinner.js
+++ src/components/Layout/LoadingSpinner.js
@@ -2,14 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CircularProgress, Box } from '@mui/material';
 
-const LoadingSpinner = ({ size = 100 }) => (
+const LoadingSpinner = ({ size }) => (
   <Box sx={{ display: 'flex', 'justify-content': 'center' }}>
     <CircularProgress size={size} />
   </Box>
 );
 
 LoadingSpinner.propTypes = {
-  size: PropTypes.number.isRequired,
+  size: PropTypes.number,
+};
+
+LoadingSpinner.defaultProps = {
+  size: 100,
 };
 
 export default LoadingSpinner;
